4132f5684190d918ac7af8937ddd06ade2fcdafd
abhinavcode <cancrackjee2014@gmail.com>
2017-12-12 18:09:04 +0530

    landing v1

---
+++ app/src/main/java/com/thirio/android/adapter/TravelViewPagerAdapter.java
@@ -0,0 +1,40 @@
+package com.thirio.android.adapter;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+
+import com.thirio.android.fragments.TravelExpandingFragment;
+import com.thirio.android.model.Travel;
+import com.thirio.android.utils.ExpandingViewPagerAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+/**
+ * Created by Qs on 16/5/30.
+ */
+public class TravelViewPagerAdapter extends ExpandingViewPagerAdapter {
+
+    List<Travel> travels;
+
+    public TravelViewPagerAdapter(FragmentManager fm) {
+        super(fm);
+        travels = new ArrayList<>();
+    }
+
+    public void addAll(List<Travel> travels){
+        this.travels.addAll(travels);
+        notifyDataSetChanged();
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        Travel travel = travels.get(position);
+        return TravelExpandingFragment.newInstance(travel);
+    }
+
+    @Override
+    public int getCount() {
+        return travels.size();
+    }
+
+}
